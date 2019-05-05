d745b7745a2cf2e872965dfd95e908ab88b9fa15
brainer <40418544+brainer3220@users.noreply.github.com>
2019-05-05 20:17:32 +0900

    제조사의 스마트폰 찾기
    알고리즘 수정중, LG전자 추가.

--- code/ComponyOfSmartPhone.js
+++ code/ComponyOfSmartPhone.js
@@ -2,12 +2,20 @@ module.exports.function = function ComponyOfSmartPhone (comp) {
   const SmartPhoneData = require("./data/SmartPhone_Data.js");
   const console = require("console");
 
+  // 삼성전자 SmartPhone 모음.
   const Samsung_Galaxy_S = require("./data/PhoneData/SmartPhone/Samsung/Galaxy_S.js")  // Galaxy S Series File
   const Samsung_Galaxy_Note = require("./data/PhoneData/SmartPhone/Samsung/Galaxy_Note.js")  // Galaxy Note Series File
   const Samsung_Galaxy_Fold = require("./data/PhoneData/SmartPhone/Samsung/Galaxy_Fold.js")  // Galaxy Fold Series File
   const Samsung_Galaxy_A = require("./data/PhoneData/SmartPhone/Samsung/Galaxy_A.js")  // Galaxy A Series File
   const Smasung_Galaxy_M = require("./data/PhoneData/SmartPhone/Samsung/Galaxy_M.js")  // Galaxy M Series File
   
+  // LG전자 SmartPhone 모음.
+  const Lg_G = require("./data/PhoneData/SmartPhone/LG/G_Series.js") // LG G Series File
+  const Lg_Pro = require("./data/PhoneData/SmartPhone/LG/Pro_Series.js")  // LG Pro Seires File
+  const Lg_V = require("./data/PhoneData/SmartPhone/LG/V_Series")  // LG Pro Series File
+  
+  // Google SmartPhone 모음
+  const Google_Pixel = require(".data/PhoneData/SmartPhoneData/Google/Pixel.js")  // Google Pixel Series File
   var PhoneData = new Array;
   let lent = 0;
   
@@ -26,7 +34,17 @@ module.exports.function = function ComponyOfSmartPhone (comp) {
         PhoneData.push(Samsung_Galaxy_A[i].name)
       }
     }
-    else if()
+    else if(Lg_G[0].comp == String(comp)) {
+      for(let i = 0; i < Lg_G.length; i++)  {
+        PhoneData.push(Lg_G[i].name)
+      }
+      for(let i = 0; i < Lg_Pro.length; i++)  {
+        PhoneData.push(Lg_Pro[i].name)
+      }
+      for(let i = 0; i < Lg_V.length; i++)  {
+        PhoneData.push(Lg_V[i].name)
+      }
+    }
   }
   
   console.log(lent)
